a3b92b81cc5592ff0560f48045ccebd1f58071e3
Isaiah Harris <harrisfambam@gmail.com>
2018-06-08 20:57:21 -1000

    complete scope assignment

--- scope.js
+++ scope.js
@@ -2,11 +2,11 @@
 var isCookieJarOpen = null;
 
 function closeLid() {
-  /* answer here */
+  isCookieJarOpen = false;
 }
 
 function openLid() {
-  /* answer here */
+  isCookieJarOpen = true;
 }
 
 
@@ -17,7 +17,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + ' ' +  world;
   }
   return innerFunction();
 }
@@ -31,8 +31,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix.length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
@@ -46,19 +46,18 @@ function sendDataToClient() {
   var userObject = {
     handle: 'neo',
     authenticated: false
-  }
+  };
+  var userObject2 = {
+    handle: 'morpheus',
+    authenticated: false
+  };
 
   function authenticateUser(obj, username) {
-    var userObject = {
-      handle: 'morpheus',
-      authenticated: false
-    };
-
-    if (userObject.handle === username) {
-      userObject.authenticated = true;
-      return userObject
+    if (obj.handle === 'neo') {
+      obj.authenticated = true;
+      return obj;
     } else {
-      return userObject
+      return obj;
     }
   }
   authenticateUser(userObject, 'neo')
